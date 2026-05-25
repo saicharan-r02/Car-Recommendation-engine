cac5231961390ffd92605985c87fdfbf00033c88
saicharan-r02 <saicharanr227@gmail.com>
2026-05-25 22:59:51 +0530

    server file changes

--- backend/server.js
+++ backend/server.js
@@ -6,34 +6,25 @@ const path = require('path');
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
-
 app.post('/api/recommend', (req, res) => {
 
     const { time, price } = req.body;
-    
     const scriptPath = path.join(__dirname, 'CR-Backend.py');
-
     const python = spawn('python', [scriptPath, time, price]);
-    
     let output = "";
     python.stdout.on('data', (data) => output += data.toString());
     python.stderr.on('data', (data) => console.error("Python Error:", data.toString()));
 
     python.on('close', (code) => {
-
         try {
             res.json(JSON.parse(output));
         } 
-        
         catch (e) {
             res.json([]);
         }
-
     });
 });
 
 const PORT = 5001;
-
 app.listen(PORT, () => console.log(`Backend running on Port ${PORT}`));
