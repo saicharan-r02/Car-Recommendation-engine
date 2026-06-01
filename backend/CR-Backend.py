2c061d9354aca367f104363d654ba2c8c9f7e80b
saicharan <saicharanr227@gmail.com>
2026-06-01 23:08:35 +0530

    Improve score calculation for affordable cars

--- backend/CR-Backend.py
+++ backend/CR-Backend.py
@@ -18,12 +18,10 @@ def get_recommendations(u_time, u_price):
     affordable = df[df["Price (in USD)"] <= u_price].copy()
 
     if affordable.empty: return []
-
     p_max = df["Price (in USD)"].max()
     t_max = df["0-60 MPH Time (seconds)"].max()
 
     affordable["score"] = (abs(affordable["Price (in USD)"] - u_price) / p_max * 0.8)+(abs(affordable["0-60 MPH Time (seconds)"] - u_time) / t_max * 0.2)
-
     affordable = affordable.sort_values("score")
     unique_cars = affordable.drop_duplicates(subset=["Car Name", "Car Model"])
 
